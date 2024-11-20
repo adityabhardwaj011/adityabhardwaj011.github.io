775ea93b32045c0394b870817c5d1758916bcfda
adityabhardwaj011 <adityab790@gmail.com>
2024-11-20 17:51:29 +0530

    new

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+    {
+        "name":"Product 1",
+        "desc":"This is the description of product 1",
+        "price":999,
+        "url":"https://picsum.photos/id/77/450/300"
+    },
+    {
+        "name":"Product 2",
+        "desc":"This is the description of product 2",
+        "price":10,
+        "url":"https://picsum.photos/id/77/450/300"
+    }
+]
+);
