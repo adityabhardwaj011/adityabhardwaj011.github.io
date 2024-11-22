640ea52c88bdb8aac25098c2db3b6052cbbb1037
adityabhardwaj011 <adityab790@gmail.com>
2024-11-22 18:03:31 +0530

    new

--- temp.js
+++ temp.js
@@ -1,14 +1,14 @@
 db.products.insertMany(
     [
     {
-        "name":"Product 1",
-        "desc":"This is the description of product 1",
+        "name":"Product 25",
+        "desc":"This is the description of product 25",
         "price":999,
         "url":"https://picsum.photos/id/77/450/300"
     },
     {
-        "name":"Product 2",
-        "desc":"This is the description of product 2",
+        "name":"Product 29",
+        "desc":"This is the description of product 29",
         "price":10,
         "url":"https://picsum.photos/id/77/450/300"
     }
